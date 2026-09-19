44c30508db8141fe5552842b6f8a7a9ad73b4692
David <davidmel7562@gmail.com>
2026-09-19 23:11:04 +0200

    fix: fix components.json + add new rule for migration + migration test + update ROM extensions

---
+++ backend/prisma/migrations/20260919210245_add_sha1_md5_data/migration.sql
@@ -0,0 +1,30 @@
+/*
+  Warnings:
+
+  - Made the column `embedding` on table `RomEmbedding` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- AlterEnum
+-- This migration adds more than one value to an enum.
+-- With PostgreSQL versions 11 and earlier, this is not possible
+-- in a single migration. This can be worked around by creating
+-- multiple migrations, each migration adding only one value to
+-- the enum.
+
+
+ALTER TYPE "IdentificationSource" ADD VALUE 'DAT_SHA1_DATA';
+ALTER TYPE "IdentificationSource" ADD VALUE 'DAT_MD5_DATA';
+
+-- DropIndex
+DROP INDEX "romembedding_embedding_hnsw";
+
+-- AlterTable
+ALTER TABLE "Rom" ADD COLUMN     "headerBytesSkipped" INTEGER NOT NULL DEFAULT 0,
+ADD COLUMN     "md5Data" TEXT,
+ADD COLUMN     "sha1Data" TEXT;
+
+-- AlterTable
+ALTER TABLE "RomEmbedding" ALTER COLUMN "embedding" SET NOT NULL;
+
+-- CreateIndex
+CREATE INDEX "Rom_userId_sha1Data_idx" ON "Rom"("userId", "sha1Data");
